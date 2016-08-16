7918b0466e2d499950d7d7f42b10f28e6b8bbbdc
octopussy <uberpilot@gmail.com>
2016-08-16 22:26:07 +0300

    Initial

---
+++ android/src/com/borschlabs/xcom/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.borschlabs.xcom;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.borschlabs.xcom.XcomGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new XcomGame(), config);
+	}
+}
