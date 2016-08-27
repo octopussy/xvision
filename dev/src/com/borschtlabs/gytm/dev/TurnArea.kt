9900ea0906de4a6b3d9527bc826ea8a41fd1aa56
octopussy <uberpilot@gmail.com>
2016-08-27 21:53:15 +0300

    refactoring

--- dev/src/com/borschtlabs/gytm/dev/TurnArea.kt
+++ dev/src/com/borschtlabs/gytm/dev/TurnArea.kt
@@ -58,25 +58,25 @@ class TurnArea private constructor(val waypoints: List<WayPoint>, level: Level) 
 
             val centerShift = (unitSize - 1) * 0.5f
 
-            var current = WayPoint(startCell, 0, Vector2(startCell.x + centerShift, startCell.y + centerShift))
-
-            visited.add(current)
-            frontier.add(current)
-            distanceMap.put(current, maxDistance)
+            fun addWP(wp: WayPoint, distance: Int) {
+                frontier.add(wp)
+                visited.add(wp)
+                distanceMap[wp] = distance
+            }
 
             fun extendFrontier(from: WayPoint, x: Int, y: Int, distance: Int) {
                 val n = level.getCell(x, y)
                 val alreadyVisited = visited.find { it.cell == n } == null
                 if (n != null && alreadyVisited && !level.checkCellsIfOccupied(unitSize, n.x, n.y)) {
-                    val wp = WayPoint(n, visited.size, Vector2(n.x + centerShift, n.y  + centerShift))
-
+                    val wp = WayPoint(n, visited.size, Vector2(n.x + centerShift, n.y + centerShift))
                     from.connections.add(DefaultConnection(from, wp))
-
-                    frontier.add(wp)
-                    visited.add(wp)
-                    distanceMap[wp] = distance
+                    addWP(wp, distance)
                 }
             }
+
+            var current = WayPoint(startCell, 0, Vector2(startCell.x + centerShift, startCell.y + centerShift))
+
+            addWP(current, maxDistance)
 
             while (frontier.isNotEmpty()) {
                 current = frontier.removeAt(0)!!
